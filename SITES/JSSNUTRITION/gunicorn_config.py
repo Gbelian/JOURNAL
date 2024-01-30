07ddb0fdbfdba40de569c279df642b89effedd8e
SImonJ09 <jude-simon.agohoundje@epitech.eu>
2024-01-30 10:00:58 +0100

    corrige erreur de deploiement

--- SITES/JSSNUTRITION/gunicorn_config.py
+++ SITES/JSSNUTRITION/gunicorn_config.py
@@ -3,10 +3,12 @@ workers = 4
 
 import os
 
+import whitenoise
+
 from JSSNUTRITION.settings import STATIC_URL
 
 from JSSNUTRITION.wsgi import application
 
-application = WhiteNoise(application, root=STATIC_URL)
+application = whitenoise(application, root=STATIC_URL)
 
 STATIC_URL = '/static/'
