a6ff122629b073fefe6216c4cf9343cfdab53522
SImonJ09 <jude-simon.agohoundje@epitech.eu>
2024-02-14 18:49:31 +0100

    update

--- SITES/JSSNUTRITION/gunicorn_config.py
+++ SITES/JSSNUTRITION/gunicorn_config.py
@@ -3,14 +3,15 @@ workers = 4
 
 import os
 from whitenoise import WhiteNoise
-from JSSNUTRITION.settings import BASE_DIR, STATIC_URL
 
+from JSSNUTRITION.settings import BASE_DIR, STATIC_URL
 from JSSNUTRITION.wsgi import application
 
+# Utilisation de WhiteNoise pour servir les fichiers statiques de manière efficace
 application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'static'))
 
-# Serve static files directly by WhiteNoise
+# Configuration pour servir les fichiers statiques avec un préfixe défini par STATIC_URL
 application.add_files(os.path.join(BASE_DIR, 'static'), prefix=STATIC_URL)
 
-# Enable Gzip compression for served files
+# Activation de la compression Gzip pour les fichiers statiques
 application.add_files(os.path.join(BASE_DIR, 'static'), prefix=STATIC_URL, gzip=True)
